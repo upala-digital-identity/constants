29a582189397e4cfebd54cc957ea467f8eae877a
Petr Porobov <porobov.p@gmail.com>
2021-11-05 13:34:33 +0400

    Now Upala constants is a class

--- index.js
+++ index.js
@@ -1,29 +1,99 @@
 const fs = require('fs')
+const _ = require('lodash')
 const path = require('path');
+const ethers = require('ethers')
+const FormatTypes = ethers.utils.FormatTypes
+const chalk = require('chalk')
 
-function getAbis() {
-    return JSON.parse(
-        fs.readFileSync(getAbisFilePath()))
+function loadAbis() {
+  return JSON.parse(
+    fs.readFileSync(abisPath()))
 }
 
-function getAddresses(args) {
-    try {
-        return JSON.parse(fs.readFileSync(getAddressesFilePath(args)))
-      } catch (err) {
-        return {}
-      }
+function loadAddresses(chainID) {
+  try {
+    return JSON.parse(fs.readFileSync(addressesPath(chainID)))
+  } catch (err) {
+    return {}
+  }
 }
 
-function getAbisFilePath() {
-    return path.join(__dirname, '/constants/abis.json')
+// todo remove __dirname - make it simpler. 
+function abisPath() {
+  return path.join(__dirname, '/constants/abis.json')
 }
 
-function getAddressesFilePath(args) {
-    let filename = args.chainID.toString() + '_addresses.json'
-    return path.join(__dirname, '/constants/', filename)
+function addressesPath(chainID) {
+  let filename = chainID.toString() + '_addresses.json'
+  return path.join(__dirname, '/constants/', filename)
 }
 
-exports.getAbisFilePath = getAbisFilePath
-exports.getAddressesFilePath = getAddressesFilePath
-exports.getAbis = getAbis
-exports.getAddresses = getAddresses
+
+class UpalaConstants {
+  constructor(chainID, options = {skipLoadFromDisk: false}) {
+    this.chainID = chainID
+    if (!options.skipLoadFromDisk)
+      this.abis = loadAbis()
+      this.addresses = loadAddresses(chainID)
+  }
+
+  /*******************
+  WRITING TO CONSTANTS
+  ********************/
+  // this is Upala admin part (don't use this anywhere else)
+
+  addContract(contractName, contract) {
+    this.abis[contractName] = contract.interface.format(FormatTypes.json)
+    this.addresses[contractName] = contract.address
+  }
+
+  addABI(contractName, abi) {
+    this.abis[contractName] = abi
+  }
+
+  // saves abis and addresses on disk (only if difference found with existing)
+  save() {
+    // Export ABIs
+    let savedAbis = loadAbis()
+    if (!_.isEqual(savedAbis, this.abis)) { 
+      console.log(chalk.red('\n\n\nWarning ABIs changed.\n\n\n'))
+      fs.writeFileSync(abisPath(), JSON.stringify(this.abis))
+    }
+    // Export addresses
+    let savedAddresses = loadAddresses(this.chainID)
+    if (!_.isEqual(savedAddresses, this.addresses)) {
+      fs.writeFileSync(addressesPath(this.chainID), JSON.stringify(this.addresses))
+      console.log('Wrote addresses to:', chalk.green(addressesPath(this.chainID)))
+    }
+  }
+
+/*******************
+READING FROM CONSTANTS
+********************/
+
+  // loads contract from constants or attaches given address to an ABI by name
+  getContract(contractName, wallet, address) {
+    if (address) {
+      // e.g. used for dai and pools - attaching
+      return new ethers.Contract(
+        address, 
+        this.abis[contractName], 
+        wallet)
+    } else {
+      return new ethers.Contract(
+        this.addresses[contractName], 
+        this.abis[contractName], 
+        wallet)
+    }
+  }
+
+  getAddress(contractName) {
+      return this.addresses[contractName]
+  }
+
+  getAbi(contractName) {
+      return this.abis[contractName]
+  }
+}
+
+exports.UpalaConstants = UpalaConstants
