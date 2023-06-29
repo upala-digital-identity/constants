76ed9e4001fe6fa8935a44a927df3d24f9576363
Petr Porobov <porobov@users.noreply.github.com>
2023-06-29 12:18:58 +0400

    Typescript compatibility

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+declare module '@upala/constants' {
+    export const UpalaConstants: any;
+    export const validNetworkID: any;
+    export const numConfirmations: any;
+    export const getDaiAddress: any;
+    export const isTestNetwork: any;
+  }
+  
